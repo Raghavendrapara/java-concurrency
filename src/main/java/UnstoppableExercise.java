110c3fe558cd3e6d0ed3cb357b7c4dea9553fb22
raghavendra <raghavendra0360@gmail.com>
2025-12-13 12:31:11 +0530

    conceptual exercise for interrupts

--- src/main/java/UnstoppableExercise.java
+++ src/main/java/UnstoppableExercise.java
@@ -1,2 +1,27 @@
+import java.util.Random;
+
 public class UnstoppableExercise {
+    public static void main(String[] args) throws InterruptedException {
+        Thread worker = new Thread(() -> {
+            Random random = new Random();
+
+            while (true) {
+                // Heavy CPU work
+                Math.sin(random.nextDouble());
+
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Worker: Signal received! Stopping.");
+                    break;
+                }
+            }
+        });
+
+        worker.start();
+
+        System.out.println("Main: Let it run for 10ms...");
+        Thread.sleep(10);
+
+        System.out.println("Main: Interrupting now!");
+        worker.interrupt(); // This sets a boolean flag = true
+    }
 }
